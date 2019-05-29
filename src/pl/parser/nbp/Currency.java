a746c0300f408a371affbe057ff67ff43de3f125
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-05-29 22:43:42 +0200

    Declared atributes in classes

---
+++ src/pl/parser/nbp/Currency.java
@@ -0,0 +1,11 @@
+package pl.parser.nbp;
+
+public class Currency {
+
+    private String currencyCode;
+
+    private float buyRate;
+
+    private float sellRate;
+
+}
--- src/pl/parser/nbp/MainClass.java
+++ src/pl/parser/nbp/MainClass.java
@@ -3,6 +3,9 @@ package pl.parser.nbp;
 public class MainClass {
 
     public static void main(String [] args) {
+        String currencyCode = args[0];
+        String startDate = args[1];
+        String endDate = args[2];
 
     }
 }
