7f97b7fcd27e23b5f5b22c65e730e83023bf278b
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-06-01 20:42:43 +0200

    Substituted for each for lambda

--- src/main/java/pl/parser/nbp/Util/MathStatistics.java
+++ src/main/java/pl/parser/nbp/Util/MathStatistics.java
@@ -1,15 +1,13 @@
 package pl.parser.nbp.Util;
 
+import java.util.Arrays;
 import java.util.stream.DoubleStream;
 
 public class MathStatistics {
 
     public static double stdDeviation(double[] population) {
         double mean = MathStatistics.avg(population);
-        double sumPowSquared = 0;
-        for (double v : population) {
-            sumPowSquared += Math.pow(v - mean, 2);
-        }
+        double sumPowSquared = Arrays.stream(population).map(v -> Math.pow(v - mean, 2)).sum();
         return Math.sqrt(sumPowSquared / (population.length - 1));
     }
 
