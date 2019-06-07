df9735bbbf097892f9feeb3ebbaf75e85e310f9d
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-06-07 19:11:13 +0200

    Added route for several charts

--- src/main/java/pl/parser/nbp/RateChart/CurrencyRateChartController.java
+++ src/main/java/pl/parser/nbp/RateChart/CurrencyRateChartController.java
@@ -9,7 +9,11 @@ import pl.parser.nbp.ChartFile.ChartFileBucket;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.stream.Collectors;
 
 @RestController
 public class CurrencyRateChartController {
@@ -23,5 +27,17 @@ public class CurrencyRateChartController {
         file.load();
         CurrencyRateChart chart = file.getChart();
         return chart;
+    }
+
+    @GetMapping("/currency-charts/{start-date}/{end-date}/{type}")
+    public List<CurrencyRateChart> getCurrencyRateCharts(@PathVariable("start-date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate, @PathVariable("end-date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate, @PathVariable("type") char type) {
+        int startYear = Integer.parseInt(publicationDateFormat.format(startDate));
+        int endYear = Integer.parseInt(publicationDateFormat.format(endDate));
+        ChartFileBucket bucket = new ChartFileBucket(startYear, endYear);
+        ChartFile fileStart = bucket.findFile(startDate, type);
+        ChartFile fileEnd = bucket.findFile(endDate, type);
+        SortedSet<ChartFile> files = bucket.getFiles().subSet(fileStart, fileEnd);
+        files.forEach(ChartFile::load);
+        return files.stream().filter(file -> file.getType() == 'c').map(ChartFile::getChart).collect(Collectors.toList());
     }
 }
