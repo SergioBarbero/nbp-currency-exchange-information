d092aa77399c2283ebddbc4121058a68d3586414
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2020-02-09 11:00:38 +0100

    Tests for Util classes added

--- src/test/java/pl/parser/nbp/Util/FileUtilTest.java
+++ src/test/java/pl/parser/nbp/Util/FileUtilTest.java
@@ -1,4 +1,16 @@
 package pl.parser.nbp.Util;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FileUtilTest {
+
+    @Test
+    void shouldReadContentFromUrl() throws IOException {
+        String s = FileUtil.readContentFromUrl("https://www.google.com");
+        assertThat(s).isNotBlank();
+    }
 }
